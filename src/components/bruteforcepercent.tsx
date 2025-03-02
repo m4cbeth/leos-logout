ba43ef32adf614ea0464b3e7a19f4ac835b9e2ef
m4cbeth <56993541+m4cbeth@users.noreply.github.com>
2025-03-02 12:06:46 -0700

    Sun, Mar  2, 2025 12:06:46 PM

--- src/components/bruteforcepercent.tsx
+++ src/components/bruteforcepercent.tsx
@@ -10,12 +10,37 @@ export default function PercentInput() {
 
     const handleFocus = () => {
         setInput("0.00%")
-        const input = document.getElementById('percentinput') as HTMLInputElement
-        const length = input.value.length;
-        input.setSelectionRange(length-1, length-1);
     }
 
+    const handleKeyDown = (e) => {
+        if (e.key == 'Backspace') {
+            console.log()
+        }
+    }
+
+    const handleChange = (e) => {
+        console.log(e)
+        if (e.key == "Backspace") {
+            console.log("it's working iITSOKW OWRKING")
+            // get state
+            const current = input.split('')
+            const backspaced = current.slice(0,current.length-1).join("")
+            setInput(backspaced)
+            return
+        }
+        const val = e.target.value
+        const enteredDigit = val.slice(val.length - 1) // Get last character typed
+        // get input from state
+        const current = input.split("")
+        const numberArray = current.filter(char => /^[0-9]$/.test(char))
+        numberArray.push(enteredDigit)
+        const numbers = numberArray.join('')
+        const numericValue = parseFloat(numbers) / 100
+        const percent = `${numericValue.toFixed(2)}%`
+        setInput(percent)
+        }
+
     return (
-        <Input id={"percentinput"} inputMode="numeric" value={input} onFocus={handleFocus} />
+        <Input onKeyDown={handleKeyDown} inputMode="numeric" value={input} onFocus={handleFocus} onChange={handleChange} />
     )
 }
