c10393a5f1fc4d075a25ec6aa53abda3c9c7ffc5
m4cbeth <56993541+m4cbeth@users.noreply.github.com>
2025-03-02 13:14:04 -0700

    finished percent I thinkkwikcommitkwikcommit bruteforce workingkwikcommit!

--- src/components/bruteforcepercent.tsx
+++ src/components/bruteforcepercent.tsx
@@ -9,25 +9,28 @@ export default function PercentInput() {
     const [input, setInput] = useState("")
 
     const handleFocus = () => {
-        setInput("0.00%")
+        if (input == "") {
+            setInput("0.00%")
+        }
     }
 
     const handleKeyDown = (e) => {
         if (e.key == 'Backspace') {
-            console.log()
+            e.preventDefault()
+            // get state            
+            const current = input.replace('%', '').replace('.', '').split('')
+            const backspaced = current.slice(0,current.length-1).join("")
+            const numericValue = parseFloat(backspaced) / 100
+            const percent = `${numericValue.toFixed(2)}%`
+            console.log({current, backspaced, numericValue, percent})
+            setInput(percent)
+            return
         }
     }
 
+    const formattedInput = `${(parseInt(input) / 100).toFixed(2)}%`
+
     const handleChange = (e) => {
-        console.log(e)
-        if (e.key == "Backspace") {
-            console.log("it's working iITSOKW OWRKING")
-            // get state
-            const current = input.split('')
-            const backspaced = current.slice(0,current.length-1).join("")
-            setInput(backspaced)
-            return
-        }
         const val = e.target.value
         const enteredDigit = val.slice(val.length - 1) // Get last character typed
         // get input from state
@@ -41,6 +44,13 @@ export default function PercentInput() {
         }
 
     return (
-        <Input onKeyDown={handleKeyDown} inputMode="numeric" value={input} onFocus={handleFocus} onChange={handleChange} />
+        <Input
+            placeholder="0.00%"
+            onKeyDown={handleKeyDown}
+            inputMode="numeric"
+            value={input}
+            onFocus={handleFocus}
+            onChange={handleChange}
+        />
     )
 }
